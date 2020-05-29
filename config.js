8cb52b61240370f9f736823fd465d3aa7a592a06
Mario Beltrán Alarcón <belco90@gmail.com>
2020-05-29 23:01:19 +0200

    Update path config

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-readonly/`, // This path is subpath of your hosting https://domain/portfolio
   authorName: 'Anubhav Srivastava',
   heading: "I got reprogrammed by a rogue AI and now I'm totally cray",
   // social
