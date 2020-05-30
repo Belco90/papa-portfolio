32d1c0cbc580721481a782a365fbbf287491f7a0
Mario Beltrán Alarcón <belco90@gmail.com>
2020-05-30 13:03:58 +0200

    Improve avatar

--- src/components/SideBar/Header.js
+++ src/components/SideBar/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function Footer({ title, heading, avatar }) {
+export default function Header({ title, heading, avatar }) {
   return (
     <header>
       <span className="image avatar">
-        <img src={avatar} alt="" />
+        <img src={avatar} alt="Foto de Cristóbal Beltrán" />
       </span>
       <h1 id="logo">
         <a href="/#">{title}</a>
