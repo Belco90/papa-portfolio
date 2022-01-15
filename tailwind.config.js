d88471f622b4dc0666569b17d6d4d48a99289fef
Mario Beltrán Alarcón <belco90@gmail.com>
2022-01-15 13:21:57 +0100

    add brand color token

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: colors.red,
+      },
+    },
   },
   plugins: [],
 }
