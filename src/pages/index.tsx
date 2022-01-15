4ad5d8178e99103a8edc7e1038b793c782a78a12
Mario Beltrán Alarcón <belco90@gmail.com>
2022-01-15 19:34:19 +0100

    add highlights section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,11 @@ import {
 } from 'react-icons/fa'
 
 import banner from '@app-public/images/banner.jpeg'
+import pictureWorkshop from '@app-public/images/picture-workshop.jpeg'
+import pictureWestSideStory from '@app-public/images/picture-west-side-story.jpeg'
+import pictureCabaretCadiz from '@app-public/images/picture-cabaret-cadiz.jpeg'
+import pictureCabaretMalaga from '@app-public/images/picture-cabaret-malaga.jpeg'
+import pictureChef from '@app-public/images/picture-chef.jpeg'
 
 const SKILLS_COLLECTION: Array<{ icon: ReactNode; title: string }> = [
   { icon: <FaStar />, title: 'Animación' },
@@ -39,7 +44,6 @@ const HomePage = () => {
           </h1>
           <p className="text-3xl font-bold">Animación turística y hostelería</p>
         </div>
-
         <section id="about" className="space-y-5">
           {/* TODO: show profilePic when sidebar is hidden */}
           <p>
@@ -81,9 +85,7 @@ const HomePage = () => {
             entusiasmando.
           </p>
         </section>
-
         <hr className="my-8 border-2" />
-
         <section id="skills">
           <h2 className="text-3xl font-bold mb-6">Habilidades</h2>
 
@@ -112,6 +114,70 @@ const HomePage = () => {
               </Link>
             </li>
           </ul>
+        </section>
+        <hr className="my-8 border-2" />
+
+        <section id="highlights">
+          <h2 className="text-3xl font-bold mb-6">Destacados</h2>
+
+          <figure>
+            <Image
+              src={pictureWorkshop}
+              alt="Cristóbal de joven en el taller de decoración"
+              placeholder="blur"
+              className="rounded-md"
+            />
+            <figcaption className="italic">
+              En el taller, dónde aprendí diferentes técnicas de pintura y
+              decoración.
+            </figcaption>
+          </figure>
+          <hr className="my-4 border-1" />
+          <figure>
+            <div className="grid md:grid-cols-2 gap-3 items-center mb-1">
+              <Image
+                src={pictureWestSideStory}
+                alt="Cristóbal con su compañero en 'West Side Story'"
+                placeholder="blur"
+                className="rounded-md"
+              />
+              <Image
+                src={pictureCabaretCadiz}
+                alt="Cristóbal caracterizado para 'Cabaret Español'"
+                placeholder="blur"
+                className="rounded-md"
+              />
+            </div>
+            <figcaption className="italic">
+              Representando cabarets en hotel Atlanterra Sol, Zahara de los
+              Atunes (Cádiz)
+            </figcaption>
+          </figure>
+          <hr className="my-4 border-1" />
+          <figure>
+            <Image
+              src={pictureCabaretMalaga}
+              alt="Cristóbal junto a su compañera caracterizados para el cabaret"
+              placeholder="blur"
+              className="rounded-md"
+            />
+            <figcaption className="italic">
+              Cabaret 2 Pax. Transhotel Puerto S.A. Fuengirola (Málaga)
+            </figcaption>
+          </figure>
+          <hr className="my-4 border-1" />
+          <figure>
+            <Image
+              src={pictureChef}
+              alt="Cristóbal junto al jefe de cocina, con los maîtres a ambos lados"
+              placeholder="blur"
+              className="rounded-md"
+            />
+            <figcaption className="italic">
+              Con el Jefe de cocina y los Maitres de Transhotel Puerto S.A.
+              Fuengirola (Málaga)
+            </figcaption>
+          </figure>
         </section>
       </main>
     </div>
