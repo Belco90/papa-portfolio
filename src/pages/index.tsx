80b8aece798348cf14e9f6d61caf974fbda9da5c
Mario Beltrán Alarcón <belco90@gmail.com>
2022-01-15 13:46:25 +0100

    add main section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,82 +1,64 @@
-import Head from 'next/head'
-
-export default function Home() {
+const HomePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <div className="py-10 italic text-center bg-amber-200">
+        header bg will be placed here
+      </div>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className="container mx-auto px-5 text-gray-700">
+        <div className="my-10">
+          <h1 className="text-brand-500 text-5xl font-bold mb-1">
+            Cristóbal Beltrán
+          </h1>
+          <p className="text-3xl font-bold">Animación turística y hostelería</p>
         </div>
-      </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+        <section className="space-y-5 text-lg">
+          {/* TODO: show profilePic when sidebar is hidden */}
+          <p>
+            Hola, navegante. Soy Cristóbal y quiero hablarte un poco sobre mí y
+            mi trayectoria.
+          </p>
+
+          <p>
+            Mi largo recorrido como animador turístico me ha permitido adquirir
+            conocimientos del sector en general y de las relaciones públicas en
+            particular.
+          </p>
+
+          <p>
+            Tengo experiencia a la hora de confeccionar programas de animación,
+            actividades deportivas, infantiles, tercera edad, y conocimientos y
+            ejecución de todo tipo de música de baile.
+          </p>
+
+          <p>
+            Al mismo tiempo he desarrollado el trabajo de
+            facturación/restaurante, que me ha permitido ampliar mis
+            conocimientos sobre este departamento e igualmente sobre otros, como
+            por ejemplo recepción. Esto me ha posibilitado ejercer como
+            recepcionista en unos apartamentos vacacionales durante los últimos
+            meses.
+          </p>
+
+          <p>
+            Tengo dotes para el dibujo y la pintura, actividades que considero
+            mi hobby, e indudablemente un suplemento para mi trabajo. Además
+            tengo buenas ideas para cualquier tipo de decoración.
+          </p>
+
+          <p>
+            Aparte de estos conocimientos, me gustaría resaltar la base
+            principal de mi trabajo: las relaciones públicas, el llamado “don de
+            gentes”. La relación diaria con los clientes es algo que me sigue
+            entusiasmando.
+          </p>
+        </section>
+
+        <hr className="my-4 border-2" />
+      </main>
     </div>
   )
 }
+
+export default HomePage
