14a1ead839841d17eeff762a7686b49ca80420de
Mario Beltrán Alarcón <belco90@gmail.com>
2022-01-16 14:45:19 +0100

    set document language

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 const MyDocument = () => {
   return (
-    <Html>
+    <Html lang="es">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=optional"
