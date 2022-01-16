848b7bfba92ed3f5852b4ba847b1368bc822f791
Mario Beltrán Alarcón <belco90@gmail.com>
2022-01-16 20:51:49 +0100

    move next-seo config file to correct folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
-import { DefaultSeoConfig } from '~/pages/next-seo.config'
+import { DefaultSeoConfig } from '~/next-seo.config'
 import '~/styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
