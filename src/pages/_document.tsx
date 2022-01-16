b29cba637f08d657627959b6724eb2147a0ebc3e
Mario Beltrán Alarcón <belco90@gmail.com>
2022-01-16 19:33:04 +0100

    improve scroll padding for desktop

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 const MyDocument = () => {
   return (
-    <Html lang="es" className="scroll-pt-12">
+    <Html lang="es" className="scroll-pt-12 lg:scroll-pt-6">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=optional"
