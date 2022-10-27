84bea76d49c437389c513b5017d495155d26c07a
Mario Beltrán Alarcón <belco90@gmail.com>
2022-10-27 17:55:21 +0200

    Migrate _document.tsx and _app.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,22 @@
 import { FC, ReactNode } from 'react'
 import { Lato } from '@next/font/google'
+import VercelAnalytics from '~/components/VercelAnalytics'
+import { DefaultSeo } from 'next-seo'
+import { DEFAULT_SEO_CONFIG } from '~/next-seo.config'
+import { DefaultToastOptions, Toaster } from 'react-hot-toast'
 
 const lato400 = Lato({ weight: '400', variable: '--lato400-font' })
 const lato700 = Lato({ weight: '700', variable: '--lato700-font' })
+
+const DEFAULT_TOASTER_OPTIONS: DefaultToastOptions = {
+	position: 'bottom-center',
+	success: {
+		duration: 3000,
+	},
+	error: {
+		duration: 5000,
+	},
+}
 
 interface IRootLayoutProps {
 	children: ReactNode
@@ -14,7 +28,15 @@ const RootLayout: FC<IRootLayoutProps> = ({ children }) => {
 			lang="es"
 			className={`scroll-pt-12 lg:scroll-pt-6 ${lato400.className} ${lato700.className}`}
 		>
-			<body>{children}</body>
+			<head>
+				<link rel="icon" href="/favicon.ico" />
+			</head>
+			<body>
+				<VercelAnalytics />
+				<DefaultSeo {...DEFAULT_SEO_CONFIG} />
+				<Toaster toastOptions={DEFAULT_TOASTER_OPTIONS} />
+				{children}
+			</body>
 		</html>
 	)
 }
